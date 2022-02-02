fb922c39f89c151ee227258e3834fd305bcdb079
mradulovic988 <upss070288@gmail.com>
2022-02-02 23:05:27 +0100

    First push

---
+++ party-planer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Party Planer
+ *
+ * @package           Party_Planer
+ * @author            Marko Radulovic
+ * @copyright         2022 Marko Radulovic
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Party Planer
+ * Plugin URI:        https://wordpress.org/plugins/party-planer
+ * Description:       Party Planer Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cumque, dolor, dolorem dolores est eveniet expedita fugiat ipsa ipsam laudantium neque nihil obcaecati odit omnis perspiciatis quod totam velit vitae?
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.1
+ * Author:            Marko Radulovic
+ * Author URI:        https://mlab-studio.com/
+ * Text Domain:       party-planer
+ * Domain Path:       /languages
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
